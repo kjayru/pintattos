d893abc1f43431b90f832d19b9c186086dd91da8
kjayru <wiltinoco@gmail.com>
2014-08-22 07:58:48 -0500

    files de proyecto
    pentattos

---
+++ index.php
@@ -0,0 +1,552 @@
+<!DOCTYPE html>
+<html lang="en" xml:lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>
+Pintattoo - Explore All Instagram Online        
+    </title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta http-equiv="Content-Language" content="en" />
+    <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+    <meta name="viewport" content="width=970, maximum-scale=1.0" />
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <link rel="stylesheet" type="text/css" href="css/lib.css">
+</head>
+<body>
+
+    <div style="display: none">Explore Instagram online with the best experience, follow interesting people, post comment and share photo to Facebook, twitter and all other websites.</div>
+
+    <div class="bodyContent">
+
+
+
+<div class="multiViewTopBannerWrapper">
+
+    <div class="multiViewTopBanner">
+
+
+
+        <div class="navLeft">
+
+            <div class="navButtons">
+
+
+
+<div class='splitter'></div><a href='/'  class='currentPage' ><img src='img/nav_home.png' alt='' /></a>
+
+                <div class='splitter'></div><a href='javascript:void(0);' class='navButtonExplore'  ><img src='img/nav_globe.png' alt='' /></a>
+
+            </div>
+  </div>
+          
+        <div class="navButtons navRight" style="left: 115px;">
+
+            
+
+<div class='splitter'></div><a href='/Account/LoginWithService?serviceType=instagram&redirectUrl=%2F'   ><img src='img/nav_user.png' alt='Sign in with Instagram' /><span>Sign in with Instagram</span></a>
+
+
+
+
+
+
+
+<div class='splitter'></div>
+
+<div class='splitter'></div><a href='https://pinsta.zendesk.com/hc' target='_blank'  ><img src='img/nav_help.png' alt='' /></a>            <div class='splitter'></div>
+
+        </div>
+
+    </div>
+
+    
+
+    
+
+    
+
+    
+
+    
+
+</div>
+
+<div class="navButtonTip ui-tooltip-top">navButtonTip</div>
+
+
+
+<div class="explorePopover navPopover">
+
+    <div class="exploreSection">
+
+        <a href="/popular" >Popular</a>
+
+        <a href="/interesting" >Random</a>
+
+        <a href="/justnow" >Just now</a>
+
+    </div>
+
+    <div class="exploreSection">
+
+            <a class="" href="/Channel/fashion" >Fashion</a>
+
+            <a class="" href="/Channel/entertainment" >Entertainment</a>
+
+            <a class="" href="/Channel/food" >Food</a>
+
+            <a class="" href="/Channel/animal" >Pets</a>
+
+            <a class="" href="/Channel/art" >Art</a>
+
+            <a class="" href="/Channel/urban" >Urban</a>
+
+            <a class="" href="/Channel/nature" >Nature</a>
+
+            <a class="" href="/Channel/tech" >Tech</a>
+
+    </div>
+
+</div>
+
+
+
+<ul class="dropdown-menu sharePopover navPopover">
+
+    <li class="divider"></li>
+
+        <li><a href="javascript:share('facebook')" rel="nofollow"><i class="icon_bg icon_facebook"></i>Facebook</a></li>
+
+        <li><a href="javascript:share('pinterest')" rel="nofollow"><i class="icon_bg icon_pinterest"></i>Pinterest</a></li>
+
+        <li><a href="javascript:share('twitter')" rel="nofollow"><i class="icon_bg icon_twitter"></i>Twitter</a></li>
+
+        <li><a href="javascript:share('tumblr')" rel="nofollow"><i class="icon_bg icon_tumblr"></i>Tumblr</a></li>
+
+        <li><a href="javascript:share('googleplus')" rel="nofollow"><i class="icon_bg icon_googleplus"></i>Google+</a></li>
+
+        <li><a href="javascript:share('qrcode')" rel="nofollow"><i class="icon_bg icon_qrcode"></i>Scan Code</a></li>
+
+        <li><a href="javascript:share('sinaweibo')" rel="nofollow"><i class="icon_bg icon_sinaweibo"></i>Sina Weibo</a></li>
+
+        <li><a href="javascript:share('douban')" rel="nofollow"><i class="icon_bg icon_douban"></i>Douban</a></li>
+
+        <li><a href="javascript:share('qzone')" rel="nofollow"><i class="icon_bg icon_qzone"></i>QZone</a></li>
+
+        <li><a href="javascript:share('renren')" rel="nofollow"><i class="icon_bg icon_renren"></i>Renren</a></li>
+
+        <li class="divider"></li>
+
+        <li><a class="addthis_button_expanded">Other services</a></li>    
+
+</ul>
+<div id="qrCodeModal" style="z-index: 1090" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+
+    <div class="modal-header">
+
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+
+        <h3 class="scanQrCode" >Scan the QR code to get the link for the photo</h3>
+
+        <h3 class="scanQrCodeForInstagramHook">Open the following link with Safari to see the photo in Instagram</h3>
+
+    </div>
+
+    <div class="modal-body">
+
+        <div style="height: 150px; position: relative">
+
+            <img id="loadingQrCode" style="z-index:1091; position:absolute; top:65px; left:245px;" src="/Content/MultiView/ajax-loader.gif" alt="loading" />
+
+            <img id="qrCodeImage" style="z-index: 1092; position: absolute; left: 190px;" src="" alt="QR code" />
+
+        </div>
+
+        <div><a href="#" class="link" target="_blank">#</a></div>
+
+    </div>
+
+    
+
+</div>
+
+
+
+<div class="modal hide fade" id="loginFormModal" style="border:none;">
+
+    <div class="modal-header" style="background: #46749a; color: #fff; -webkit-border-radius: 5px 5px 0px 0px; border-radius: 5px 5px 0px 0px;">
+
+        <button type="button" class="close" data-dismiss="modal" style="color: #fff; text-shadow: none;">
+
+            ×</button>
+
+        <h3>Sign In</h3>
+
+    </div>
+
+    <div class="modal-body">
+
+        <div class="loginFormBody">
+
+            <div style="color: #68AABE;">
+
+                <i class="icon-info-sign" style="opacity: 0.3; margin-right: 5px;"></i>Sign in to follow other user, post comments and likes.
+
+            </div>
+
+            <div class="instagramLogin">
+
+                <a href="/Account/LoginWithService?serviceType=instagram&amp;redirectUrl=%2F">
+
+                        <img src="/img/login_instagram_en.png" alt="login instagram" />
+
+                </a>
+
+                
+
+            </div>
+
+            <div style="color: #68AABE; padding-bottom: 10px; margin-top: 24px; padding-top: 24px; border-top: solid 1px #eee;">
+
+                No Instagram account? <a href="http://instagr.am/" target="_blank">Download Instagram and sign up</a>
+
+            </div>
+
+            
+
+        </div>
+
+    </div>
+
+</div>
+    <div class="note ">
+
+        <div class="noteContent">
+
+            <div>
+
+
+
+                <a data-toggle="modal" href="/Account/LoginWithService?serviceType=instagram&amp;redirectUrl=%2F" target="_blank">
+
+                        <img src="/img/pinsta-splash_en.png" alt="pinsta introduction splash" />
+
+                </a>
+
+            </div>
+
+            <div class="hideNoteButtonContainer">
+
+                    <a class="signInLink" href="/Account/LoginWithService?serviceType=instagram&amp;redirectUrl=%2F" rel="nofollow">
+
+                            <img src="/img/login_instagram_en.png" alt="login instagram" />
+
+                    </a>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class="wallToolbar" style="text-align: center; margin-top: 14px;">
+
+        <div class="btn-group photoSizeSwitch" data-toggle="buttons-radio">
+
+            <button type="button" class="btn btn-small smallBricksBtn"><i class="icon-th"></i>S</button>
+
+            <button type="button" class="btn btn-small mediumBricksBtn"><i class="icon-th-large"></i>M</button>
+
+            <button type="button" class="btn btn-small fullBricksBtn"><i class="icon-picture"></i>L</button>
+
+            
+
+        </div>
+
+        <div class="btn-group viewModeSwitch" data-toggle="buttons-radio">
+
+            <button type="button" class="btn btn-small viewModeTightBtn" onclick="switchViewMode('tight')"><i class="icon-th"></i>Hide comments</button>
+
+            <button type="button" class="btn btn-small active viewModeBricksBtn" onclick="switchViewMode('bricks')"><i class="icon-asterisk"></i>Show comments</button>
+
+        </div>
+
+        <div class="btn-group mapModeSwitch" data-toggle="buttons-radio" style="display:none;" style="display:none;">
+
+            <button type="button" class="btn btn-small mapModeOn mapModeOnButtonStyle"><i class="icon-map-marker"></i>Map Mode</button>
+
+            <button type="button" class="btn btn-small mapModeOff" style="display:none;"><i class="icon-minus"></i>隐藏地图</button>
+
+        </div>
+
+    </div>
+
+
+
+
+
+
+
+        <div class="middleAdsBannerWrapper">
+
+            <div class="middleAdsBanner underNavBar">
+
+                <div class="adsContentWrapper">
+
+
+                </div>
+
+            </div>
+
+        </div>
+
+    
+
+    <div class="likedAlert">
+
+        <div class="likedAlertContent">
+
+            <img src="/img/heart.png" alt="" />
+
+        </div>
+
+    </div>
+
+    <div class="pageLoading">
+
+        <img src="img/page-load.gif" alt="loading" />
+
+    </div>
+
+
+
+    <div class="mapModeWrapper"><div class="mapModeMapView"></div><div style="position:absolute;top:0px; right:0px;">[x]</div></div>
+
+    <div class="mapModeWrapperPlaceholder"></div>
+
+
+
+    <div class="wall smallBrick" id="photos" style="height: 1000px;">
+
+        <div class="goTopButton">
+
+            <a href="javascript:scrollToElement($('.bodyContent'))" title="Go to top" rel="nofollow">
+
+                <img src="/img/goTopButton.png" alt="Go to top" />
+
+            </a>
+
+        </div>
+      <div class="brick" id="" mediaid="" actioninit="true" style="left: 75px; top: 14px; z-index: 100;">
+                <div class="brickBody">
+<!--- head post --->
+                        <div class="user owner clearfix poster">
+                            <div class="profileImage">
+                                <a href="/frankieromustdie" title="frnkiero" target="_blank">
+                                    <img src="http://images.ak.instagram.com/profiles/profile_265180138_75sq_1359046138.jpg">
+                                </a>
+                            </div>
+                            <div class="userName">
+                                <a href="/frankieromustdie" title="frnkiero" target="_blank">frankieromustdie </a>
+                            </div>
+                        </div>
+<!--- end head post -->
+                    <div class="pic">
+
+                        <div class="brickActions" style="display: none;">
+                            <a onmousedown="like('790085311270104999_265180138')" class="brickLike btn btn-danger" title="Double-click on the photo also likes">
+                                <i class="icon-heart icon-white"></i><span class="aText">Like</span>
+                            </a>
+                            <a onmousedown="showBrickShareMenu(this)" class="btn btn-primary dropdown-toggle" href="javascript:void(0)" title="Share" rel="nofollow"><i class="icon-share-alt icon-white"></i><span class="aText">Share</span></a>
+                            <a onmousedown="comment('790085311270104999_265180138')" class="btn btn-success" title="Comment">
+                                <i class="icon-comment icon-white"></i><span class="aText">Comment</span>
+                            </a>
+                                                    </div>
+                                                    
+                        <div class="thumb-load">
+                            <img src="/Content/MultiView/pic-load.gif" alt="Loading">
+                        </div>
+                        
+                        <a class="picLink">
+                            <img class="picThumb" src="http://scontent-a.cdninstagram.com/hphotos-xap1/t51.2885-15/927116_261599654049285_1289087967_a.jpg" alt="">
+                            <img class="picThumb picFull" srcx="http://scontent-a.cdninstagram.com/hphotos-xap1/t51.2885-15/927116_261599654049285_1289087967_n.jpg" alt="">
+                        </a>
+                        
+                    </div>
+		<!--- texto ingresado --->
+                    <div class="caption emojstext">
+                    The More You Know...                    
+                    </div>
+		<!---- texto end --->
+        <!--- status----->
+                    <div class="stats">
+                        <span class="likesCount">6903</span>
+                            <span class="commentsCount">277</span>
+                                                                        <span class="createTime">2 hours ago</span>
+                    </div>
+                               
+        <!---- fin status --> 
+        <!---- comentarios -->             
+                       <div class="comments">
+                                <div class="user clearfix">
+                                    <div class="profileImage">
+                                        <a href="/thestripedlunchkit" title="Jackie :P" target="_blank">
+                                            <img src="http://photos-c.ak.instagram.com/hphotos-ak-xpa1/925583_1475234266050354_1994998513_a.jpg">
+                                        </a>
+                                    </div>
+                                    <div class="commentRight">
+                                        <div class="userName">
+                                            <a href="/thestripedlunchkit" title="Jackie :P" target="_blank">thestripedlunchkit </a>
+                                        </div>
+                                        <div class="commentText emojstext">Fun fact <a href="/aman" target="_blank">@aman</a>._.duh</div>
+
+                                    </div>
+                                </div>
+                                
+                        </div>
+        <!--fin comentarios --->
+                </div>
+                <div class="shadow"></div>
+            </div>           
+  </div>
+</div>
+
+    <ul class="dropdown-menu shareDropDown">
+
+            <li><a href="javascript:share('facebook')" rel="nofollow"><i class="icon_bg icon_facebook"></i>Facebook</a></li>
+
+            <li><a href="javascript:share('twitter')" rel="nofollow"><i class="icon_bg icon_twitter"></i>Twitter</a></li>
+
+            <li><a href="javascript:share('pinterest')" rel="nofollow"><i class="icon_bg icon_pinterest"></i>Pinterest</a></li>
+
+            <li><a href="javascript:share('tumblr')" rel="nofollow"><i class="icon_bg icon_tumblr"></i>Tumblr</a></li>
+
+            <li><a href="javascript:share('googleplus')" rel="nofollow"><i class="icon_bg icon_googleplus"></i>Google+</a></li>
+
+            <li><a href="javascript:share('qrcode')" rel="nofollow"><i class="icon_bg icon_qrcode"></i>Scan Code</a></li>
+
+            <li><a href="javascript:share('sinaweibo')" rel="nofollow"><i class="icon_bg icon_sinaweibo"></i>Sina Weibo</a></li>
+
+            <li><a href="javascript:share('douban')" rel="nofollow"><i class="icon_bg icon_douban"></i>Douban</a></li>
+
+            <li><a href="javascript:share('qzone')" rel="nofollow"><i class="icon_bg icon_qzone"></i>QZone</a></li>
+
+            <li><a href="javascript:share('renren')" rel="nofollow"><i class="icon_bg icon_renren"></i>Renren</a></li>
+
+    </ul>
+
+
+
+    
+
+    <div class="loadingContentWrapper">
+
+        <div class="loadingContent">
+
+            <img src="/Content/MultiView/ajax-loader_2.gif" alt="loading" />
+
+        </div>
+
+    </div>
+
+    <div class="modal hide" id="mediaDetail">
+
+        <div class="modal-body">
+
+            <div class="mediaDetailContainer"></div>
+
+        </div>
+
+    </div>
+
+
+
+
+
+
+
+<div class="emojiInput popover left">
+
+    <div class="arrow"></div>
+
+    <!-- /Content/EmojiInputPopoverContent.html -->
+
+    <h3 class="popover-title">
+
+        <ul class="nav nav-tabs" id="emojiInputTab">
+
+            <li class="active"><a href="#emoji_face" data-toggle="tab"><span class="emoji emoji1f604" code="1f604" code="1f604" code="1f604"></span>
+
+            </a></li>
+
+            <li><a href="#emoji_flower" data-toggle="tab"><span class="emoji emoji1f338" code="1f338" code="1f338" code="1f338"></span>
+
+            </a></li>
+
+            <li><a href="#emoji_bell" data-toggle="tab"><span class="emoji emoji1f514" code="1f514"></span>
+
+            </a></li>
+
+            <li><a href="#emoji_car" data-toggle="tab"><span class="emoji emoji1f697" code="1f697"></span>
+
+            </a></li>
+
+            <li><a href="#emoji_mark" data-toggle="tab"><span class="emoji emoji2320e3" code="2320e3"></span>
+
+            </a></li>
+
+        </ul>
+
+    </h3>
+
+    <div class="popover-content">
+
+        <div class="emojiInputWrapper">
+
+            <div class="emojiInputContent">
+
+                <div class="tab-content">
+
+                    <div class="tab-pane active" id="emoji_face">
+
+                    </div>
+
+                    <div class="tab-pane" id="emoji_flower">
+
+                    </div>
+
+                    <div class="tab-pane" id="emoji_bell">
+
+                    </div>
+
+                    <div class="tab-pane" id="emoji_car">
+
+                    </div>
+
+                    <div class="tab-pane" id="emoji_mark">
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+
+
+</div>
+
+<div class="emojiInputPopoverContent" style="display: none;"></div>
+
+</div>
+ <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+<script src="js/main.js"></script>
+<!--<script src="js/lib.js"></script>-->
+
+</body>
+
+</html>
+
+>
